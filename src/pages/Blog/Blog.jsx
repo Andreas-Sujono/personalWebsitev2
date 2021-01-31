d77b8dd35549146de59a971eb8ea41e66e31517c
AndreasSujono <andreassujono@dathena.io>
2021-02-01 00:39:07 +0800

    fix bug

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import Parser from 'rss-parser';
 import TopNav from 'components/TopNav';
 import BlogCard from 'components/BlogCard';
+import LoadingBar from 'components/shared/LoadingBar';
+import logo from 'assets/Logo.png';
 import './style.scss';
 
 const parser = new Parser({});
@@ -9,25 +11,40 @@ const parser = new Parser({});
 function Blog() {
   const [mediumMeta, setMediumMeta] = useState({});
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getMediumData();
   }, []);
 
   const getMediumData = async () => {
-    const url = 'https://medium.com/feed/@andreassujono';
+    const url =
+      'https://cors-anywhere.herokuapp.com/https://medium.com/feed/@andreassujono';
     const feed = await parser.parseURL(url);
     console.log(feed);
 
     setMediumMeta(feed);
     setArticles(feed.items);
+    setIsLoading(false);
   };
 
   return (
     <div className="blog-page">
+      <img className="logo" src={logo} alt="andreas Logo" />
+
       <TopNav />
-      <div className="blog-header" />
+      <a href={mediumMeta.link} target="__blank">
+        <div className="blog-header">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Medium_logo_Monogram.svg/1200px-Medium_logo_Monogram.svg.png"
+            className="medium-logo"
+            alt="medium"
+          />
+          <h1>{mediumMeta.description}</h1>
+        </div>
+      </a>
       <div className="blog-content row">
+        {isLoading && <LoadingBar />}
         {articles.map((item) => (
           <div className="col-lg-4 col-md-6 col-12" key={item.title}>
             <BlogCard
