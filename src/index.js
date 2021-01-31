b54f5294645f5bdc5562ae996dc6dbca9bb13751
AndreasSujono <andreassujono@dathena.io>
2021-01-31 23:51:20 +0800

    add GA

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './App';
+
+ReactGA.initialize('G-1DNZP0ZK9X');
 
 ReactDOM.render(<App />, document.getElementById('root'));
